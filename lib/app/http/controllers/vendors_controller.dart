ab3aacc67a4edb0f427f90f1dca7f0b25860cd3d
pponnek <rbudi6823@gmail.com>
2024-12-13 13:19:32 +0700

    vendors controller

--- lib/app/http/controllers/vendors_controller.dart
+++ lib/app/http/controllers/vendors_controller.dart
@@ -1,9 +1,14 @@
 import 'package:vania/vania.dart';
+import 'package:appcoba/app/models/vendors.dart';
 
 class VendorsController extends Controller {
 
      Future<Response> index() async {
-          return Response.json({'message':'Hello World'});
+      final listVendors = await Vendors().query().get();
+      return Response.json({
+        'message' : 'data list vendors',
+        'data'    : listVendors,
+      });
      }
 
      Future<Response> create() async {
@@ -11,11 +16,53 @@ class VendorsController extends Controller {
      }
 
      Future<Response> store(Request request) async {
-          return Response.json({});
+
+      try {
+        
+        request.validate({
+          'vend_name'    : 'required',
+          'vend_address' : 'required',
+          'vend_kota'    : 'required',
+          'vend_state'   : 'required',
+          'vend_zip'     : 'required',
+          'vend_country' : 'required',
+        });
+
+        final vendorsData = request.only([
+          'vend_name',
+          'vend_address',
+          'vend_kota',
+          'vend_state',
+          'vend_zip',
+          'vend_country',
+        ]);
+
+        vendorsData['created_at'] = DateTime.now().toIso8601String();
+
+        await Vendors().query().insert(vendorsData);
+
+        return Response.json({
+          'message' : 'data vendors berhasil ditambahkan',
+          'data ' : vendorsData,
+        }, 201);
+
+      } catch (e) {
+        
+        return Response.json({
+          'message' : 'terjadi kesalahan',
+          'error'   : e.toString(),
+        });
+      }
+
      }
 
-     Future<Response> show(int id) async {
-          return Response.json({});
+     Future<Response> show(Request request, int id) async {
+
+      final vendors = await Vendors().query().where('vend_id', '=', id).first();
+
+      return Response.json({
+        'data' : vendors,
+      });
      }
 
      Future<Response> edit(int id) async {
@@ -23,11 +70,76 @@ class VendorsController extends Controller {
      }
 
      Future<Response> update(Request request,int id) async {
-          return Response.json({});
+      try {
+        request.validate({
+          'vend_name'    : 'required',
+          'vend_address' : 'required',
+          'vend_kota'    : 'required',
+          'vend_state'   : 'required',
+          'vend_zip'     : 'required',
+          'vend_country' : 'required',
+        });
+
+        final vendorsData = request.only([
+          'vend_name',
+          'vend_address',
+          'vend_kota',
+          'vend_state',
+          'vend_zip',
+          'vend_country',
+        ]);
+
+        vendorsData['updated_at'] = DateTime.now().toIso8601String();
+
+        final vendors = await Vendors().query().where('vend_id', '=', id).first();
+
+        if (vendors == null) {
+          return Response.json({
+            'message' : 'vendors dengan ID $id tidak ditemukan',
+          }, 404);
+        }
+
+        await Vendors().query().where('cust_id', '=', id).update(vendorsData);
+
+        return Response.json({
+          'message' : 'vendors berhasil diupdate',
+          'data'    : vendorsData,
+        });
+
+      } catch (e) {
+
+        return Response.json({
+          'message': 'Terjadi kesalahan',
+          'error': e.toString(),
+        }, 500);
+      }
      }
 
      Future<Response> destroy(int id) async {
-          return Response.json({});
+
+      try {
+        final vendors = await Vendors().query().where('vend_id', '=', id).first();
+
+        if (vendors == null) {
+          
+          return Response.json({
+            'message' : 'data vendor ID $id tidak ditemukan',
+          }, 404);
+        }
+
+        await Vendors().query().where('vend_id', '=', id).delete();
+
+        return Response.json({
+          'message' : 'data vendor ID $id berhasil dihapus',
+        }, 200);
+
+      } catch (e) {
+
+        return Response.json({
+          'message' : 'terjadi kesalahan',
+          'error' : e.toString(),
+        }, 500);
+      }
      }
 }
 
