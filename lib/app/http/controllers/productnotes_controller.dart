6480a05cabf6efbb87631d02e984dfacfbcca7bb
pponnek <rbudi6823@gmail.com>
2024-12-13 15:00:36 +0700

    productnotes controller

--- lib/app/http/controllers/productnotes_controller.dart
+++ lib/app/http/controllers/productnotes_controller.dart
@@ -1,9 +1,16 @@
 import 'package:vania/vania.dart';
+import 'package:appcoba/app/models/productnotes.dart';
 
 class ProductnotesController extends Controller {
 
      Future<Response> index() async {
-          return Response.json({'message':'Hello World'});
+      final listProductsnote = await Productnotes().query().get();
+
+      return Response.json({
+        'message' : 'list data product notes ditemukan',
+        'data'    : listProductsnote,
+      });
+
      }
 
      Future<Response> create() async {
@@ -11,11 +18,44 @@ class ProductnotesController extends Controller {
      }
 
      Future<Response> store(Request request) async {
-          return Response.json({});
+          try {
+
+            request.validate({
+              'prod_id'   : 'required',
+              'note_text' : 'required'
+            });
+
+            final productnotesData = request.only([
+              'prod_id',
+              'note_text',
+            ]);
+
+            productnotesData['prod_id'] = int.parse(productnotesData['prod_id']);
+            productnotesData['note_date'] = DateTime.now().toIso8601String();
+            productnotesData['created_at'] = DateTime.now().toIso8601String();
+
+            await Productnotes().query().insert(productnotesData);
+
+            return Response.json({
+              'message' : 'data product note berhasil ditambahkan',
+              'data'    : productnotesData,
+            }, 201);
+
+          } catch (e) {
+            return Response.json({
+              'message' : 'terjadi kesalahan',
+              'error'   : e.toString(),
+            });
+          }
      }
 
      Future<Response> show(int id) async {
-          return Response.json({});
+          final productsnotes = await Productnotes().query().where('note_id', '=', id).first();
+
+          return Response.json({
+            'message' : 'data ditemukan',
+            'data'    : productsnotes
+          });
      }
 
      Future<Response> edit(int id) async {
@@ -23,11 +63,64 @@ class ProductnotesController extends Controller {
      }
 
      Future<Response> update(Request request,int id) async {
-          return Response.json({});
+          try {
+            request.validate({
+              'prod_id'   : 'required',
+              'note_text' : 'required'
+            });
+
+            final productnotesData = request.only([
+              'prod_id',
+              'note_text'
+            ]);
+
+            final productnotes = await Productnotes().query().where('note_id', 'id', id).first();
+
+            if (productnotes == null) {
+              return Response.json({
+                'message' : 'productnote ID $id tidak ditemukan',
+              }, 404);
+            }
+
+            await Productnotes().query().update(productnotesData);
+
+            return Response.json({
+              'message' : 'productnotes ID $id berhasil diupdate',
+              'data' : productnotes,
+            }, 200);
+
+          } catch (e) {
+            return Response.json({
+              'message' : 'terjadi kesalahan',
+              'error'   : e.toString(),
+            }, 500);
+          }
      }
 
      Future<Response> destroy(int id) async {
-          return Response.json({});
+          try {
+            final productnotes = await Productnotes().query().where('note_id', '=', id).first();
+
+            if (productnotes == null) {
+              return Response.json({
+                'message' : 'product dengan id $id tidak ditemukan',
+
+              }, 404);
+            }
+
+            await Productnotes().query().where('note_id', '=', id).delete();
+
+            return Response.json({
+              'message' : 'product dengan ID $id berhasil dihapus',
+            }, 200);
+
+          } catch (e) {
+
+            return Response.json({
+              'message' : 'terjadi kesalahan saat menghapus',
+              'error'   : e.toString(),
+            });
+          }
      }
 }
 
